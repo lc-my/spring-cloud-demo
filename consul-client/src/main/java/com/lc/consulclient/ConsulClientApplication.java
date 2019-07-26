0687db5b4bdd80af816f63ec40da4b225e3e2188
liuchong <liuchong@viomi.com.cn>
2019-07-26 13:59:21 +0800

    consulclient

--- consul-client/src/main/java/com/lc/consulclient/ConsulClientApplication.java
+++ consul-client/src/main/java/com/lc/consulclient/ConsulClientApplication.java
@@ -2,8 +2,10 @@ package com.lc.consulclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsulClientApplication {
 
     public static void main(String[] args) {

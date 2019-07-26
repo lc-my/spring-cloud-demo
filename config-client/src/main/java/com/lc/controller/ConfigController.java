fbcbae3b9097debf29c3d5d7886fe1e1e4ea0b61
liuchong <liuchong@viomi.com.cn>
2019-07-26 18:53:56 +0800

    更新

--- config-client/src/main/java/com/lc/controller/ConfigController.java
+++ config-client/src/main/java/com/lc/controller/ConfigController.java
@@ -2,13 +2,17 @@ package com.lc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @auth liuchong
  * @data 2019-07-26 17:45
  **/
+@RefreshScope
 @RestController
 public class ConfigController {
 
@@ -16,7 +20,7 @@ public class ConfigController {
     @Value("${info.profile}")
     String info;
 
-    @GetMapping("/info")
+    @RequestMapping(value = "/info",method = {RequestMethod.GET,RequestMethod.POST})
     public String info(){
         return info;
     }
